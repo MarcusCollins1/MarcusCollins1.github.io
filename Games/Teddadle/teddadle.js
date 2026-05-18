17bdd56e84c8516af551646f5f196b7700bbe2ef
Marcus Collins <146639438+MarcusCollins1@users.noreply.github.com>
2026-05-18 14:16:03 +0100

    Update teddadle.js

--- Games/Teddadle/teddadle.js
+++ Games/Teddadle/teddadle.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const boardEl = document.getElementById("board");
     const keyboardEl = document.getElementById("keyboard");
 
