494f01f605a29c1a8b70f839c2e8555ef850276c
Marcus Collins <146639438+MarcusCollins1@users.noreply.github.com>
2026-05-20 17:16:13 +0100

    Initialize Firebase and add document function
    This file initializes Firebase and Firestore, sets up analytics, and provides a function to add documents to a specified collection.

---
+++ Games/Polygon/polygonFireBase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    query,
+    orderBy,
+    getDocs,
+    deleteDoc,
+    doc,
+    addDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_CXSZVz6meJgcJyktktWNmPtLmeFNXn0",
+    authDomain: "marcus-collins-github-website.firebaseapp.com",
+    projectId: "marcus-collins-github-website",
+    storageBucket: "marcus-collins-github-website.firebasestorage.app",
+    messagingSenderId: "328004594228",
+    appId: "1:328004594228:web:47074e07c446a328bbf861",
+    measurementId: "G-6M9HBX4E3Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+async function addDocument(collection, fields) {
+    await addDoc(collection(db, collection), fields);
+}
