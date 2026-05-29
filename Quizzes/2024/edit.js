3e0c2eb3c540f5d4d4e4f7611c5a98f3005c52b8
Marcus Collins <146639438+MarcusCollins1@users.noreply.github.com>
2026-05-29 17:06:40 +0100

    Add files via upload

---
+++ Quizzes/2024/edit.js
@@ -0,0 +1,24 @@
+import { getSubmissionIds } from "./editFireBase.js";
+
+const submissionSelect = document.getElementById("submissionSelect");
+
+
+async function populateSubmissionSelect() {
+    options = await getSubmissionIds();
+    options.forEach(option => {
+        const opt = document.createElement("option");
+        opt.value = option;
+        opt.textContent = option;
+        submissionSelect.appendChild(opt);
+    });
+}
+
+async function submissionSelectChange(value) {
+    console.log("Selected:", value);
+}
+
+submissionSelect.addEventListener("change", (event) => {
+    submissionSelectChange(event.target.value);
+});
+
+populateSubmissionSelect();
---
+++ Quizzes/2024/editFireBase.js
@@ -0,0 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    setDoc,
+    updateDoc,
+    serverTimestamp,
+    doc,
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_CXSZVz6meJgcJyktktWNmPtLmeFNXn0",
+    authDomain: "marcus-collins-github-website.firebaseapp.com",
+    projectId: "marcus-collins-github-website",
+    storageBucket: "marcus-collins-github-website.firebasestorage.app",
+    messagingSenderId: "328004594228",
+    appId: "1:328004594228:web:47074e07c446a328bbf861",
+    measurementId: "G-6M9HBX4E3Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+export async function getSubmissionIds() {
+    try {
+        const submissionsRef = collection(db, "quizzes", "2024", "submissions");
+        const snapshot = await getDocs(submissionsRef);
+        const submissions = snapshot.docs.map((docSnap) => ({
+            id: docSnap.id,
+            ...docSnap.data()
+        }));
+        return Object.values(submissions).map(item => item.id);
+    } catch (error) {
+        console.error(error);
+    }
+}
