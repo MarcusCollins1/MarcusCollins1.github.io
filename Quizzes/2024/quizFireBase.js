5fcc188c47986962a78f22c1d2fbdc4af1b82b37
Marcus Collins <146639438+MarcusCollins1@users.noreply.github.com>
2026-05-29 15:57:01 +0100

    Add files via upload

---
+++ Quizzes/2024/quizFireBase.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_CXSZVz6meJgcJyktktWNmPtLmeFNXn0",
+    authDomain: "marcus-collins-github-website.firebaseapp.com",
+    projectId: "marcus-collins-github-website",
+    storageBucket: "marcus-collins-github-website.firebasestorage.app",
+    messagingSenderId: "328004594228",
+    appId: "1:328004594228:web:47074e07c446a328bbf861",
+    measurementId: "G-6M9HBX4E3Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function submitQuizForm(data) {
+    try {
+        const name = data.name;
+
+        const submissionsRef = collection(db, "quizzes", "2024", "submissions");
+        const snapshot = await getDocs(submissionsRef).docs.map((docSnap) => ({
+            id: docSnap.id,
+            ...docSnap.data()
+        }));
+        console.log(snapshot);
+    } catch (error) {
+        console.error(error);
+    }
+}
