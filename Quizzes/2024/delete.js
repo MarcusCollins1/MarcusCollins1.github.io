76490163dbabe9ea71c6969c39f857b04953a45f
Marcus Collins <146639438+MarcusCollins1@users.noreply.github.com>
2026-05-30 14:31:38 +0100

    Add files via upload

---
+++ Quizzes/2024/delete.js
@@ -0,0 +1,33 @@
+import {
+    getNames,
+} from "./deleteFireBase.js"
+const nameList = document.getElementById("nameList");
+
+async function populateNameList() {
+    const names = await getNames();
+    names.forEach(name => {
+        const li = document.createElement("li");
+        li.className = "name-item";
+        
+        const span = document.createElement("span");
+        span.textContent = name;
+        li.appendChild(span);
+
+        const btn = document.createElement("button");
+        btn.className = "delete-button";
+        btn.textContent = "Delete";
+        btn.onclick = function () {
+
+        };
+        li.appendChild(btn);
+
+        nameList.appendChild(li);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (prompt("Enter password:") === "delete") {
+        populateNameList();
+        document.body.style.display = "block";
+    }
+});
