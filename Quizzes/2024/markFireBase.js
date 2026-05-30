922c6f498a48bd42efcc4873b5d2fefec68b0da4
Marcus Collins <146639438+MarcusCollins1@users.noreply.github.com>
2026-05-30 10:25:36 +0100

    Update markFireBase.js

---
+++ Quizzes/2024/markFireBase.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    getDoc,
+    setDoc,
+    updateDoc,
+    serverTimestamp,
+    doc,
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_CXSZVz6meJgcJyktktWNmPtLmeFNXn0",
+    authDomain: "marcus-collins-github-website.firebaseapp.com",
+    projectId: "marcus-collins-github-website",
+    storageBucket: "marcus-collins-github-website.firebasestorage.app",
+    messagingSenderId: "328004594228",
+    appId: "1:328004594228:web:47074e07c446a328bbf861",
+    measurementId: "G-6M9HBX4E3Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function getSubmissionIds() {
+    try {
+        const submissionsRef = collection(db, "quizzes", "2024", "submissions");
+        const snapshot = await getDocs(submissionsRef);
+        const submissions = snapshot.docs.map((docSnap) => ({
+            id: docSnap.id,
+            ...docSnap.data()
+        }));
+        return submissions
+            .filter(submission => !submission.marked)
+            .map(submission => submission.id);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getSubmission(id) {
+    try {
+        const submissionRef = doc(db, "quizzes", "2024", "submissions", id);
+        const snapshot = await getDoc(submissionRef);
+        if (snapshot.exists()) {
+            return snapshot.data();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
