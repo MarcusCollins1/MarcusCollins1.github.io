31a507d5cdb44f99d1816f499bde056f90c77a34
Marcus Collins <146639438+MarcusCollins1@users.noreply.github.com>
2026-06-12 23:00:09 +0100

    Update mapominoes.js

--- Games/Mapominoes/mapominoes.js
+++ Games/Mapominoes/mapominoes.js
@@ -13,6 +13,20 @@ import {
     arrayRemove,
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyA_CXSZVz6meJgcJyktktWNmPtLmeFNXn0",
+    authDomain: "marcus-collins-github-website.firebaseapp.com",
+    projectId: "marcus-collins-github-website",
+    storageBucket: "marcus-collins-github-website.firebasestorage.app",
+    messagingSenderId: "328004594228",
+    appId: "1:328004594228:web:47074e07c446a328bbf861",
+    measurementId: "G-6M9HBX4E3Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
 export const el = {
     returnBtn: document.getElementById("returnBtn"),
 
