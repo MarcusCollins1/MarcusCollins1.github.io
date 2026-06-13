424b1f823d2b4df4c2839d02a7f4fb5c1ecca2a1
Marcus Collins <146639438+MarcusCollins1@users.noreply.github.com>
2026-06-13 23:12:38 +0100

    Update mapominoes.js

--- Games/Mapominoes/mapominoes.js
+++ Games/Mapominoes/mapominoes.js
@@ -386,7 +386,7 @@ function renderHand() {
         });
         el.playerHand.appendChild(transitElement);
     }
-    el.playerHand.style.display = "block";
+    el.playerHand.style.display = "flex";
 }
 
 function removeCardFromHandByName(cardName) {
