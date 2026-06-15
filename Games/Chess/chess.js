c9c4d4394f0946eff92fd1f2fa0afa5adb864ab3
Marcus Collins <mjdj27123@gmail.com>
2026-06-15 03:27:22 +0100

    Add chessboard initialization and rendering functions in chess.js

--- Games/Chess/chess.js
+++ Games/Chess/chess.js
@@ -3,3 +3,36 @@ const el = {
 
     chessTable: document.getElementById("chessTable"),
 };
+
+let setInUse = "Standard";
+
+let board = [];
+
+function resetBoard() {
+    board = [
+    ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+    ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+    ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+    ];
+}
+
+function renderBoard() {
+    if (board.length === 0) return;
+    for (let rowIdx = 0; rowIdx < 8; rowIdx++) {
+        for (let colIdx = 0; colIdx < 8; colIdx++) {
+            const cellValue = board[rowIdx][colIdx];
+            if (cellValue === "") continue;
+            const cell = document.getElementById(`${rowIdx+1}-${colIdx+1}`);
+            cell.style.backgroundImage = `url(./Images/Pieces/${setInUse}/${cellValue})`;
+        }
+    }
+}
+
+
+resetBoard();
+renderBoard();
