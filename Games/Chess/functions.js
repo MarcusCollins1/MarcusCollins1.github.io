140301dbcd923674c34333ff4d52d87dd48044b1
Marcus Collins <mjdj27123@gmail.com>
2026-06-16 15:58:13 +0100

    Fix syntax for direction arrays in findValidMoves function for Bishop and King pieces

--- Games/Chess/functions.js
+++ Games/Chess/functions.js
@@ -16,7 +16,7 @@ export function findValidMoves(rowIdx, colIdx) {
     switch (piece) {
         // Bishop
         case "b":
-            dirs = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
+            dirs = [[-1, -1], [-1, 1], [1, -1], [1, 1]];
             for (const dir of dirs) {
                 let [newRow, newCol] = [rowIdx, colIdx];
                 while (true) {
@@ -38,7 +38,7 @@ export function findValidMoves(rowIdx, colIdx) {
             break;
         // King
         case "k":
-            dirs = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
+            dirs = [[-1, -1], [-1, 1], [1, -1], [1, 1]];
             for (const dir of dirs) {
                 const [newRow, newCol] = [rowIdx+dir[0], colIdx+dir[1]];
                 if (!((0 <= newRow < board.length) && (0 <= newCol <= board[0].length))) continue;
