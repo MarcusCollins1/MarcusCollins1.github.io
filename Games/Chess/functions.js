0abcd3ca26083a4b30d498079b9b692afd61b8db
Marcus Collins <mjdj27123@gmail.com>
2026-06-16 15:47:16 +0100

    Fix typo in variable name for cellValue assignment in findValidMoves function

--- Games/Chess/functions.js
+++ Games/Chess/functions.js
@@ -4,7 +4,7 @@ import {
 } from "./chess.js"
 
 export function findValidMoves(rowIdx, colIdx) {
-    const cellValue = board[rowIDx][colIdx];
+    const cellValue = board[rowIdx][colIdx];
     if (cellValue === "") return;
     const pieceColour = cellValue[0];
     const piece = cellValue[1];
