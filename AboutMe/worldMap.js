920152f99d0e61b3d27dbbc1d770a0b6aab15289
Marcus Collins <mjdj27123@gmail.com>
2026-07-06 18:12:33 +0100

    Update visitedCountries array to include additional countries

--- AboutMe/worldMap.js
+++ AboutMe/worldMap.js
@@ -1,4 +1,4 @@
-const visitedCountries = ["GB", "BE", "CA"];
+const visitedCountries = ["BE", "CA", "CR", "HR", "CU", "DK", "FJ", "FR", "GR", "VA", "HK", "IN", "IT", "JE", "LI", "LU", "MO", "MX", "ME", "MA", "NL", "NO", "PT", "ZA", "ES", "SE", "CH", "GB", "US"];
 const values = {};
 visitedCountries.forEach(code => {
     values[code] = { color: "#8b5cf6" };
