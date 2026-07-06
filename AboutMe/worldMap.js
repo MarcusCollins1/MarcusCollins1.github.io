297e4d12afd23f229cf7cb4ce4c91b09221bf2fe
Marcus Collins <mjdj27123@gmail.com>
2026-07-06 17:29:27 +0100

    Add worldMap.js to implement SVG map for tracking visited countries

---
+++ AboutMe/worldMap.js
@@ -0,0 +1,17 @@
+new svgMap({
+    targetElementID: "svgMap",
+
+    data: {
+        data: {
+            visited: {
+                name: "Visited"
+            }
+        },
+
+        applyData: "visited",
+
+        values: {
+            GBR: { visited: 1 },
+        }
+    }
+});
