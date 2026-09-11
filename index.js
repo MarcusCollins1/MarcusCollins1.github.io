df5cc775a80b1ad859acb921c4c93c77d0ae5e34
Marcus Collins <mjdj27123@gmail.com>
2026-09-11 22:27:44 +0100

    Refactor getAOCRepoFiles function to a more generic getFileNames function for improved reusability

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-async function getAOCRepoFiles() {
+async function getFileNames(owner, repo, path="") {
     const response = await fetch(
-        "https://api.github.com/repos/MarcusCollins1/advent-of-code/contents"
+        `https://api.github.com/repos/${owner}/${repo}/contents/${path}`
     );
 
     if (!response.ok) {
         throw new Error(`GitHub API error: ${response.status}`);
     }
 
-    return await response.json();
+    const files = await response.json();
+
+    return files.map(file => file.name);
 }
 
-getAOCRepoFiles().then(files => {
-    console.log(files);
-
-    for (const file of files) {
-        console.log(file.name);
-    }
-})
+getFileNames("MarcusCollins1", "advent-of-code")
+    .then(names => console.log(names));
